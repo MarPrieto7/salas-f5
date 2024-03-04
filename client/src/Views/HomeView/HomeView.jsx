9a9648155d901d9e47abd79bcd86dd4462413157
natalia9519 <nataliayda@gmail.com>
2024-03-04 14:24:16 +0100

    Estructure of HomeView with the styles

--- client/src/Views/HomeView/HomeView.jsx
+++ client/src/Views/HomeView/HomeView.jsx
@@ -1,9 +1,34 @@
 import "./HomeView.css";
+import ImageRoom from '../../assets/image/roomimage.jpg'
 
 function HomeView() {
     return (
         <main>
-            <p>HomeView</p>
+            <img src={ImageRoom} className="home-image" alt="Imagen de una sala con mesas y sillas"/>
+            <section className="home-section">
+                <article>
+                <p><u><strong>Barcelona</strong></u><br/>
+                    Avinguda Bogatell, 82<br/>
+                    08005 Barcelona<br/>
+                    info@factoriaf5.org
+                </p>
+                <button>Ver las salas</button>
+                    </article>
+                    <article>
+                <p><u><strong>Madrid</strong></u><br/>
+                    C. Fernando Poo, 25<br/>
+                    28045 Madrid<br/>
+                    madrid@factoriaf5.org</p>
+                    <button>Ver las salas</button>
+                    </article>
+                    <article>
+                <p><u><strong>Langreo</strong></u><br/>
+                    Calle Hornos Altos, s/n<br/>
+                    33949 Asturias<br/>
+                    asturias@factoriaf5.org</p>
+                    <button>Ver las salas</button>
+                    </article>
+            </section>
         </main>
     )
 }
